22a489dcdafc1971ef4a678c807942dcdb74cdba
tion-low <tion.low.rug@gmail.com>
2019-05-20 11:07:15 +0900

    add ios demoapp

---
+++ Demo-iOS/DemoAudioEngineOnRTMP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoAudioEngineOnRTMP
+//
+//  Created by Takayuki Sei on 2019/05/20.
+//  Copyright © 2019 tionlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
